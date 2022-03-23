2f69c186903c4c2fe05009909545a23787c2ceac
niha8515 <niha8515@yahoo.com>
2022-03-23 07:40:44 +0430

    move Activity2.kt to ResultFragment.kt

--- app/src/main/java/com/example/game/GameFragment.kt
+++ app/src/main/java/com/example/game/GameFragment.kt
@@ -1,13 +1,13 @@
 package com.example.game
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.game.databinding.FragmentGameBinding
 
 
@@ -66,8 +66,9 @@ class GameFragment : Fragment() {
                     Storage.maxScore=Storage.score
                 }
                 Storage.questionNumber=1
-                val intent= Intent(activity,Activity2::class.java)
-                startActivity(intent)
+                findNavController().navigate(R.id.action_gameFragment_to_resultFragment)
+//                val intent= Intent(activity,Activity2::class.java)
+//                startActivity(intent)
             }
             else {
                 for (button in btnArray) {
--- app/src/main/java/com/example/game/ResultFragment.kt
+++ app/src/main/java/com/example/game/ResultFragment.kt
@@ -1,12 +1,20 @@
 package com.example.game
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.game.databinding.FragmentResultBinding
+import com.google.android.material.internal.ContextUtils
+import kotlin.system.exitProcess
 
 class ResultFragment : Fragment() {
+
+    lateinit var binding: FragmentResultBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +25,37 @@ class ResultFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false)
+        binding= FragmentResultBinding.inflate(layoutInflater, container, false)
+
+        initView()
+
+        return binding.root
+    }
+    @SuppressLint("SetTextI18n")
+    private fun initView() {
+        binding.scoreFinalTxv.text="your score is  ${Storage.score.toString()}"
+        binding.record .text="most score is  ${Storage.maxScore.toString()}"
+
+        binding.exitBtn.setOnClickListener {
+            Storage.questionNumber=1
+            Storage.score=0
+
+            exitApplication()
+        }
+
+
+        binding.replyBtn.setOnClickListener {
+            Storage.questionNumber=1
+            Storage.score=0
+            activity?.onBackPressed()
+//            val intent= Intent(activity,MainActivity::class.java)
+//            startActivity(intent)
+        }
+
+    }
+    @SuppressLint("RestrictedApi")
+    fun exitApplication() {
+        ContextUtils.getActivity(activity)?.finishAffinity()
     }
 
 }
